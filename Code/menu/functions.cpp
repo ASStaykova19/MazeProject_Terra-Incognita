6a91d9b88d0de7732b08de620c744b859d42b55d
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-11-03 21:53:42 +0200

    Create indexXY function

--- Code/menu/functions.cpp
+++ Code/menu/functions.cpp
@@ -12,3 +12,10 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 #define WALL_DOWN 2
 #define WALL_LEFT 3
 
+int mapHeight = 0;
+int mapWidth = 0;
+
+int indexXY(int x, int y)
+{
+	return y * mapWidth + x;
+}
