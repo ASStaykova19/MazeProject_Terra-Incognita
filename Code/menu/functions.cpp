51cec729be4721e86c26d97ad4d8706e937e6ade
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-11-03 20:58:20 +0200

    Create preprocessor macros for directions

--- Code/menu/functions.cpp
+++ Code/menu/functions.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+#define WALL_UP 0
+#define WALL_RIGHT 1
+#define WALL_DOWN 2
+#define WALL_LEFT 3
+
 struct CELL {
 	bool left;
 	bool top;
