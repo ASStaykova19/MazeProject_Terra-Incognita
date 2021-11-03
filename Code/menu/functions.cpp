42e05b83e21460ea167521818d834e71fcc0cfdd
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-11-03 22:00:02 +0200

    Create generateMap function

--- Code/menu/functions.cpp
+++ Code/menu/functions.cpp
@@ -27,3 +27,48 @@ bool checkXY(int x, int y)
 
 	return true;
 }
+
+void generateMap(int x, int y, char* grid)
+{
+	grid[indexXY(x, y)] = ' ';
+
+	int dirs[4];
+	dirs[0] = WALL_UP;
+	dirs[1] = WALL_RIGHT;
+	dirs[2] = WALL_DOWN;
+	dirs[3] = WALL_LEFT;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int r = rand() & 3;
+		int temp = dirs[r];
+		dirs[r] = dirs[i];
+		dirs[i] = temp;
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int dx = 0, dy = 0;
+
+		switch (dirs[i])
+		{
+		case WALL_UP: dy = -1; break;
+		case WALL_DOWN: dy = 1; break;
+		case WALL_RIGHT: dx = 1; break;
+		case WALL_LEFT: dx = -1; break;
+		}
+
+		int x2 = x + (dx << 1);
+		int y2 = y + (dy << 1);
+
+		if (checkXY(x2, y2))
+		{
+			if (grid[indexXY(x2, y2)] == '#')
+			{
+				grid[indexXY(x2 - dx, y2 - dy)] = ' ';
+
+				generateMap(x2, y2, grid);
+			}
+		}
+	}
+}
