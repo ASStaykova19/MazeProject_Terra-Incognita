9c44ef35d4f7e6816e4ee4184208106c8e55620a
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-11-05 21:47:02 +0200

    Declare variables for colectables and coordinations

--- Code/menu/functions.h
+++ Code/menu/functions.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <windows.h>
+#include<ctime>
+
 using namespace std;
 
 void menu();
--- Code/menu/main.cpp
+++ Code/menu/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int colectCount = 0;
+	int playerColect = 0;
+
+	int x = 1;
+	int y = 1;
+
     menu();
 }
