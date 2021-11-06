c66c098ccc93260619dec876874ac2f6841c502e
Daniel Georgiev <DZGeorgiev19@codingburgas.bg>
2021-11-06 12:49:56 +0200

    Update .h file

--- Code/menu/functions.h
+++ Code/menu/functions.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <windows.h>
-#include<ctime>
-
-using namespace std;
+#include <ctime>
 
 extern int mapHeight;
 extern int mapWidth;
 
 void menu();
+
+void beginGame();
+void enterSize();
--- Code/menu/main.cpp
+++ Code/menu/main.cpp
@@ -4,25 +4,5 @@ int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	int colectCount = 0;
-	int playerColect = 0;
-
-	int x = 1;
-	int y = 1;
-
-	char* grid = new char[mapHeight * mapWidth];
-
-	char** map = new char* [mapHeight * mapWidth];
-
-	for (int i = 0; i < mapHeight; i++)
-	{
-		map[i] = new char[mapWidth];
-	}
-
-	for (int i = 0; i < mapWidth * mapHeight; i++)
-	{
-		grid[i] = '#';
-	}
-
     menu();
 }
